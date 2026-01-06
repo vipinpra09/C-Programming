6bb7164b55ddab97a753612f853d6b254ac6cc4a
Vipin_Prajapati09 <vipinpra7348@gmail.com>
2026-01-06 11:56:09 +0530

    Add basic C program to print messages
    Implement a simple C program that prints messages using printf and puts.

---
+++ P_hello.c
@@ -0,0 +1,21 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    if(printf("Hello world"))
+    {
+        
+    }
+    while(!printf("hello world"))
+    {
+        
+    }
+if(puts("Hello vipin"))
+{
+    
+}
+switch(0){
+    case'0':puts("Hello vipin");
+}
+    return 0;
+}
