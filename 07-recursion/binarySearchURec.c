cb7620916402544692258dc361802b700c98faff
Vipin Prajapati <vipinpra7348@gmail.com>
2026-08-23 21:36:27 +0530

    Create binarySearchURec.c

---
+++ 07-recursion/binarySearchURec.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int low, int high, int key)
+{
+    if (low > high)
+        return -1;
+
+    int mid = low + (high - low) / 2;
+
+    if (arr[mid] == key)
+        return mid;
+
+    if (key < arr[mid])
+        return binarySearch(arr, low, mid - 1, key);
+
+    return binarySearch(arr, mid + 1, high, key);
+}
+
+int main()
+{
+    int arr[] = {10, 20, 30, 40, 50, 60, 70};
+    int n = 7;
+    int key, result;
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    result = binarySearch(arr, 0, n - 1, key);
+
+    if (result != -1)
+        printf("Element found at index %d\n", result);
+    else
+        printf("Element not found\n");
+
+    return 0;
+}
