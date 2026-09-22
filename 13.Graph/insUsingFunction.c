4b9b5bfbb3ad150b7aeab5235652218194fbcba4
Vipin Prajapati <vipinpra7348@gmail.com>
2026-09-22 17:49:53 +0530

    Update insUsingFunction.c

--- 13.Graph/insUsingFunction.c
+++ 13.Graph/insUsingFunction.c
@@ -4,25 +4,18 @@ void addEdges(int graph[3][3],int m,int n){
     graph[m][n]=1;
     graph[n][m]=1;
 }
-int main() {
-    
-      addEdges(graph,0,2);
+
+void displayGraph(int graph[3][3]){
     for(int i = 0 ; i < 3;i++){
         for(int j = 0 ; j < 3 ; j++){
             printf("%d ",graph[i][j]);
         }
         printf("\n");
     }
-    // int r = 3;
-    // int c = 3;
-// int graph[3][3]={0};
+}
+int main() {
     
-//     graph[0][1] = 1;
-//     graph[1][0] = 1;
+      addEdges(graph,0,2);
+      displayGraph(graph);
     
-//     graph[0][2] = 1;
-//     graph[2][0] = 1;
-    
-//     graph[0][3] = 1;
-//     graph[3][0] = 1;
 }
